49c490d87cefeefd0a2214ac53816e8583b4a3a6
Bullets-jyr <jyr@bullets.co.kr>
2024-03-20 10:58:02 +0900

    Initial commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# bloc_payload
+
+### Simple Theme Setting App
+
+- Using event payload to give additional information to Bloc
+
---
+++ lib/main.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+import 'dart:math';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Event Payload',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Theme'),
+      ),
+      body: Center(
+        child: ElevatedButton(
+          onPressed: () {
+            final int randInt = Random().nextInt(10);
+            print('randInt: $randInt');
+          },
+          child: const Text(
+            'Change Theme',
+            style: TextStyle(fontSize: 24.0),
+          ),
+        ),
+      ),
+    );
+  }
+}
